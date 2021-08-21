6786d8f4a2363f756f14bcfb5ce600bcc9d82b55
Anatoli Reviakou <an.reviakou@gmil.com>
2021-08-22 00:38:01 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;}
+
+  let minElement = array[0];
+  for (let i=0; i < array.length; i++) {
+        if (array[i] < minElement) {
+          minElement = array[i]
+        }
+      }
+  return minElement;
+    
 }
 
+
+
+
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;}
+
+  let maxElement = array[0];
+  for (let i=0; i < array.length; i++) {
+        if (array[i] > maxElement) {
+          maxElement = array[i]
+        }
+      }
+  return maxElement;
+    
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;}
+  let total = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    total+=array[i];
+  }
+  const result = total/array.length;
+
+  return result;
+
 }
